44c463ac222293949aff450f844c6f3c14a3c17c
Tkai87 <tkai@usc.edu>
2022-03-03 14:57:25 -0800

    frontend backend connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductService } from './product/shared/product.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { NavbarComponent } from './common/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,  
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
+
 
 @Component({
   selector: 'app-product-list',
@@ -9,11 +10,18 @@ import { products } from '../../products';
 })
 export class ProductListComponent implements OnInit {
 
-  products: any
+     products: any
 
-  constructor() {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.products = products
+    const productObservable = this.productService.getProducts();
+    productObservable.subscribe(
+      (data)=> { 
+        this.products = data
+    },
+      (err)=> {console.log('got value ' + err); }
+    )
+
   }
 }
